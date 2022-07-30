ee319318d3f8ace1f4e1436e2742503320a02a82
wanglei4687 <wanglei4687@gmail.com>
2022-07-30 17:17:30 +0800

    chore: update script
    Signed-off-by: wanglei4687 <wanglei4687@gmail.com>

--- linux/ubuntu.sh
+++ linux/ubuntu.sh
@@ -51,8 +51,8 @@ git-fetch-with-cli = true
 EOF
 
 # go
-sudo wget -O /tmp/go.tar.gz https://dl.google.com/go/go$version.$goos-$goarch.tar.gz 
-sudo rm -rf /usr/local/go && sudo tar -xzf /tmp/go.tar.gz
+wget -O $HOME/go.tar.gz https://dl.google.com/go/go$version.$goos-$goarch.tar.gz 
+sudo rm -rf /usr/local/go && tar -xzf $HOME/go.tar.gz
 
 mkdir .go/bin
 mv $HOME/go/bin/* $HOME/.go/bin
@@ -86,4 +86,5 @@ echo $(cargo --version --verbose)
 echo "------------------------------"
 echo $(go version)
 
-rm -rf $HOME/go
+sudo rm -rf $HOME/go
+sudo rm -rf $HOME/go.tar.gz
