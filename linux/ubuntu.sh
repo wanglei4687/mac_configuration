9cb6eb88143d8e5ddfb0a731208dd6de9d4973b4
wanglei4687 <wanglei4687@gmail.com>
2022-07-30 16:35:48 +0800

    chore: update
    Signed-off-by: wanglei4687 <wanglei4687@gmail.com>

--- linux/ubuntu.sh
+++ linux/ubuntu.sh
@@ -55,7 +55,7 @@ sudo wget -O /tmp/go.tar.gz https://dl.google.com/go/go$version.$goos-$goarch.ta
 sudo rm -rf /usr/local/go && sudo tar -xzf /tmp/go.tar.gz
 
 sudo mkdir .go .go/bin
-sudo mv $HOME/go/bin $HOME/.go/bin
+sudo mv $HOME/go/bin/* $HOME/.go/bin
 
 cat > $HOME/.go/env << EOF
 case ":${PATH}:" in
