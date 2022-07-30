b0aede4dd489f03fc324aaf5fe74a965f1cfe6ec
wanglei4687 <wanglei4687@gmail.com>
2022-07-30 16:10:22 +0800

    feat: update ubuntu config
    Signed-off-by: wanglei4687 <wanglei4687@gmail.com>

--- linux/ubuntu.sh
+++ linux/ubuntu.sh
@@ -72,8 +72,8 @@ source $HOME/.go/env
 go env -w GO111MODULE=on
 go env -w GOPROXY=https://goproxy.cn,direct
 
-echo export GOROOT=$HOME/.go >> $HOME/.profile
-echo export GOPATH=$HOME/.go >> $HOME/.profile
+echo "export GOROOT=$HOME/.go" >> $HOME/.profile
+echo "export GOPATH=$HOME/.go" >> $HOME/.profile
 
 source .bashrc
 
